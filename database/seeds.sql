50dfab0361fe48f17cd47183a527ce041477e696
Vanessa de La Cuetara <23455324+vdelacue@users.noreply.github.com>
2019-08-04 13:18:35 -0400

    initial commit with folders and dir

---
+++ database/seeds.sql
@@ -0,0 +1,9 @@
+USE notetaker_db;
+
+INSERT INTO notes (title, body)
+VALUES
+  ("Note Taker App", "Complete this homework by Tuesday, August 6, 2019 11:59 PM"),
+  ("READ THE DOCS", "Read documentation about... Node, Express, MySQL, etc."),
+  ("Mark myself present in Bootcampspot", "Remember to mark myself present every Monday-Friday at 10am"),
+  ("Commit more", "Get those commits up. It shows the process and progress.")
+  -- ADD MORE
