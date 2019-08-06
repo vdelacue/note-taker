1705b6402abf7c24bc1d87da23a8ee2904ca2c06
Vanessa de La Cuetara <23455324+vdelacue@users.noreply.github.com>
2019-08-06 18:37:09 -0400

    all html routes working api routes for post get and delete working

--- controller/api-routes.js
+++ controller/api-routes.js
@@ -6,6 +6,8 @@ const connection = require("../config/connection.js");
 // Import the model notes.js to use its database functions.
 const note = require("../models/note.js");
 
+
+//get all notes data
 api_routes.get("/api/notes", function (req, res) {
   connection.query("SELECT * FROM notes", function (err, data) {
     if (err) {
@@ -15,47 +17,49 @@ api_routes.get("/api/notes", function (req, res) {
   })
 });
 
+//get single note data according to id given in url
+api_routes.get("/api/notes/:id", function (req, res) {
+  let id = req.params.id;
+  let chosenNote = [];
+  connection.query("SELECT * FROM notes", function (err, data) {
+    if (err) {
+      return res.status(500).end();
+    }
+    for (var i = 0; i < data.length; i++) {
+      if (data[i].id === id) {
+        chosenNote.push(data[i]);
+      }
+    }
+    return res.json(chosenNote);
+  })
+});
 
+//save a new Note to the database
+api_routes.post("/api/notes", function (req, res) {
+  connection.query(
+    "INSERT INTO notes (title, body) VALUES (?, ?)", 
+    [req.body.title, req.body.body], 
+    function (err, result) {
+    if (err) {
+      // If an error occurred, send a generic server failure
+      return res.status(500).end();
+    }
+    // Send back the ID of the new note
+    res.json({
+      id: result.insertId
+    });
+  });
+})
 
+api_routes.delete("/api/notes/:id", function (req, res) {
+  connection.query(`DELETE FROM notes WHERE id = ${req.params.id}`, function (err, data) {
+    if (err) {
+      return res.status(500).end();
+    }
+    return res.json(data);
+  })
+});
 
-
-// api_routes.get("/api/notes", function (req, res) {
-//   note.all("notes", function (result) {
-//     res.json(result);
-//   });
-
-// });
-
-
-// //reate all our routes and set up logic within those routes where required.
-// api_routes.post("/api/notes", function (req, res) {
-//   note.create(["title", "body"], [req.body.name, req.body.body], function (result) {
-//     // Send back the ID of the new quote
-//     res.json({
-//       id: result.insertId
-//     });
-//   });
-// });
-
-// api_routes.put("/api/notes/:id", function (req, res) {
-//   var condition = "id = " + req.params.id;
-
-//   console.log("condition", condition);
-
-//   note.update({
-//       body: req.body.body
-//     },
-//     condition,
-//     function (result) {
-//       if (result.changedRows === 0) {
-//         // If no rows were changed, then the ID must not exist, so 404
-//         return res.status(404).end();
-//       }
-//       res.status(200).end();
-
-//     }
-//   );
-// });
 
 // Export routes for server.js to use.
 module.exports = api_routes;
--- public/assets/js/notes.js
+++ public/assets/js/notes.js
@@ -1,70 +1,79 @@
-
+$(function() {
 // target areas in notes.html using jQuery selectors, start all variable names with $ to know it is a jQuery selector variable
-const $searchNoteBtn = $("#search-btn")
-const $saveNoteBtn = $("#save-note-btn");
-const $newNoteBtn = $("#new-note-btn");
-const $noteList = $(".list-container .list-group");
-const $noteTitle = $(".note-title-text");
-const $noteBody = $(".note-body-text");
+const searchNoteBtn = $("#search-btn");
+const saveNoteBtn = $("#save-note-btn");
+const newNoteBtn = $("#new-note-btn");
+const noteList = $(".list-container .list-group");
+const noteTitle = $(".note-title-text");
+const noteBody = $(".note-body-text");
 
 //-------------------------GET AND RENDER ALL NOTES--------------------//
 //function that will be called after AJAX call retrieves JSON data
 //funciton will take JSON data and convert into html code to be rendered on page
-let renderNoteList = function (notes) {
-  $noteList.empty();
+
+let displayNotes = function (notes) {
+  noteList.empty();
 
   let noteListItems = [];
 
   for (let i = 0; i < notes.length; i++) {
     let note = notes[i];
 
-    let $li = $("<li class='list-group-item'>").data(note);
-    let $titleDiv = $("<div>");
-    let $titleSpan = $("<span class='font-weight-bold'>").text(note.title);
-    let $delBtn = $(
-      "<i class='fas fa-trash-alt delete-note ml-2'>"
-    );
+    let li = $("<li class='list-group-item'>").data(note);
+    let titleDiv = $("<div>");
+    let titleSpan = $("<span class='font-weight-bold'>").text(note.title);
+    let delBtn = $(`<i class='fas fa-trash-alt delete-note ml-2' data-id="${note.id}">`);
+    let noteP = $("<p class='mt-2'>").text(note.text);
 
-    let $noteP = $("<p class='mt-2'>").text(note.text);
+    titleDiv.append(titleSpan, delBtn);
 
-    $titleDiv.append($titleSpan, $delBtn);
-
-    $li.append($titleDiv, $noteP);
-    noteListItems.push($li);
+    li.append(titleDiv, noteP);
+    noteListItems.push(li);
   }
 
-  $noteList.append(noteListItems);
+  noteList.append(noteListItems);
 };
 
-
 //get JSON object of notes from database (api-routes returned JSON object in )
-let getAndRenderNotes = function () {
+let getJSONnotes = function () {
   $.ajax({
     url: "/api/notes",
     method: "GET"
   }).then(function (data) {
-    renderNoteList(data);
+    displayNotes(data);
   });
 };
 
+getJSONnotes();
 
+//---------------------DELETE A NOTE FROM DATABASE AND PAGE------------------------//
 
+// Delete the clicked note
+let handleNoteDelete = function (event) {
+  // Prevent the click listener for the list from being called when the button inside of it is clicked
+  event.preventDefault();
 
+  let note = $(this).data("id");
+    console.log(note)
+  // Delete the note with the id of `note.id`
+  // Render the active note
+  $.ajax({
+    url: "/api/notes/" + note,
+    method: "DELETE"
+  }).then(function () {
+    location.reload();
+  });
+};
 
+//---------------------------SAVE A NOTE AND POST TO PAGE----------------------//
 
-
-
-
-
-
-
-// Get the note data from the inputs, save it to the db and update the view
+// Get the note data from the inputs, save it to the database and update the view
 let handleNoteSave = function () {
   let newNote = {
-    title: $noteTitle.val(),
-    text: $noteText.val()
+    title: noteTitle.val(),
+    body: noteBody.val()
   };
-
+console.log(newNote)
   $.ajax({
     url: "/api/notes",
     data: newNote,
@@ -74,25 +83,7 @@ let handleNoteSave = function () {
   });
 };
 
-// Delete the clicked note
-let handleNoteDelete = function (event) {
-  // Prevent the click listener for the list from being called when the button inside of it is clicked
-  event.stopPropagation();
-
-  let note = $(this)
-    .parents(".list-group-item")
-    .data();
-
-  // Delete the note with the id of `note.id`
-  // Render the active note
-  $.ajax({
-    url: "/api/notes/" + note.id,
-    method: "DELETE"
-  }).then(function () {
-    location.reload();
-  });
-};
-
+//------------------------------SEARCH BUTTON RENDERS SINGLE PAGE WITH NOTE------------------//
 
 $("#search-btn").on("click", function() {
   var searchedNote = $("#note-search").val().trim();
@@ -116,11 +107,12 @@ $("#search-btn").on("click", function() {
 
 
 
-$searchNoteBtn.on("click", handleSearchNote)
-$saveNoteBtn.on("click", handleNoteSave);
-$noteList.on("click", ".delete-note", handleNoteDelete);
+// searchNoteBtn.on("click", handleSearchNote)
+saveNoteBtn.on("click", handleNoteSave);
+noteList.on("click", ".delete-note", handleNoteDelete);
 
 // Gets and renders the initial list of notes
-getAndRenderNotes();
+// displayNotes();
 
 //export?
+});
